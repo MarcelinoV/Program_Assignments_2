b5ce90edee98ff349f5e851b0c4cbc31691d05ca
MarcelinoV <marcelinomv98x@gmail.com>
2021-06-22 19:54:15 -0400

    first commit

---
+++ Program_Assignments_2/first_file.py
@@ -0,0 +1,24 @@
+"""print('''
+Hello World!!!
+''')"""
+
+# STEP 1
+name = 'LeBron' # string
+age = 35 # integer
+salary = 37.44 # float
+plays_basketball = True #boolean
+jersey_number = '23' # string
+
+# STEP 2
+car_name = 'Ford'
+x = 50
+my_first_name = 'Marcelino'
+
+print(type(name))
+print(type(age))
+print(type(salary))
+print(type(plays_basketball))
+print(type(jersey_number))
+print(type(car_name))
+print(type(x))
+print(type(my_first_name))
