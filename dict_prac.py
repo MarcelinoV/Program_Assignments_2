fb3f81932dcf6354b90915e0728d8eca08f9b683
MarcelinoV <marcelinomv98x@gmail.com>
2021-06-24 10:48:27 -0400

    full update

---
+++ dict_prac.py
@@ -0,0 +1,18 @@
+
+# STEP 2
+
+food_prices = {'Chicken': 1.59, 
+                'Beef': 1.99,
+                'Cheese': 1.00,
+                'Milk': 2.5}
+
+# STEP 3
+animes = {'Naruto': 2005,
+          'Full Metal Alchemist': 2004,
+          'One Piece': 2003,
+          'Yasuke': 2021}
+
+test = {2005:'Naruto'}
+
+print(test[2005])
+print(animes['Naruto'])
